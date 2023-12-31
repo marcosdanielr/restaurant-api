aa0e690d0ef52abe4d868753d4ad86cb92db6a38
marcosdanielr <marcosdaniel99@protonmail.com>
2023-12-31 18:25:09 -0300

    chore: address added in restaurants table

--- src/database/init.sql
+++ src/database/init.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS "restaurants" (
   "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
   "name" VARCHAR(255) NOT NULL,
   "image_path" VARCHAR(255),
+  "address": VARCHAR(255) NOT NULL,
   "created_at" TIMESTAMP DEFAULT NOW() NOT NULL,
   "updated_at" TIMESTAMP DEFAULT NOW() NOT NULL,
   CONSTRAINT pk_restaurants PRIMARY KEY (id)
