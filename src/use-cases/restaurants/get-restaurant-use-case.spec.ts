55dbc0775832cb6e19d1ccf3f11a79a1b51f7639
marcosdanielr <marcosdaniel99@protonmail.com>
2023-12-31 20:07:24 -0300

    test: get restaurant by id (use case)

---
+++ src/use-cases/restaurants/get-restaurant-use-case.spec.ts
@@ -0,0 +1,36 @@
+import { InMemoryRestaurantsRepository } from "@/repositories/in-memory/in-memory-restaurants-repository";
+import { describe, it, expect, beforeEach } from "vitest";
+import { GetRestaurantUseCase } from "./get-restaurant-use-case";
+
+let restaurantsRepository: InMemoryRestaurantsRepository;
+let sut: GetRestaurantUseCase;
+
+describe("Get Restaurant Use Case", () => {
+  beforeEach(() => {
+    restaurantsRepository = new InMemoryRestaurantsRepository();
+    sut = new GetRestaurantUseCase(restaurantsRepository);
+  });
+
+  it("should be able to get restaurant by id", async () => {
+
+    await Promise.all(
+      [
+        restaurantsRepository.create({
+          name: "Burguer",
+          address: "Avenida",
+        }),
+        restaurantsRepository.create({
+          name: "Lanchonete",
+          address: "Avenida 2",
+        })
+      ]
+    ); 
+
+
+    const { restaurant } = await sut.execute({
+      id: restaurantsRepository.restaurants[0].id
+    });
+
+    expect(restaurant).toEqual(restaurantsRepository.restaurants[0]);
+  });
+});
