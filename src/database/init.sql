b858b97fc5a5c901998ed97c992b9fb08e5df1b5
marcosdanielr <marcosdaniel99@protonmail.com>
2023-12-31 17:32:07 -0300

    chore: init db

---
+++ src/database/init.sql
@@ -0,0 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS "restaurants" (
+  "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+  "name" VARCHAR(255) NOT NULL,
+  "image_path" VARCHAR(255),
+  "created_at" TIMESTAMP DEFAULT NOW() NOT NULL,
+  "updated_at" TIMESTAMP DEFAULT NOW() NOT NULL,
+  CONSTRAINT pk_restaurants PRIMARY KEY (id)
+);
