4e7a08122f99a684251a93dc0b427c3db0eeec6e
marcosdanielr <marcosdaniel99@protonmail.com>
2023-12-31 19:48:39 -0300

    feat: get restaurant (use case)

---
+++ src/use-cases/restaurants/get-restaurant-use-case.ts
@@ -0,0 +1,25 @@
+import { RestaurantsRepository } from "@/repositories/restaurants-repository";
+import { Restaurant } from "@/types/repositories/restaurants-repository";
+
+type GetRestaurantUseCaseRequest = {
+  id: string
+}
+
+type GetRestaurantUseCaseResponse = {
+  restaurant: Restaurant
+}
+
+export class GetRestaurantUseCase {
+  constructor(private restaurantsRepository: RestaurantsRepository) {}
+
+  async execute({
+    id
+  }: GetRestaurantUseCaseRequest): Promise<GetRestaurantUseCaseResponse> {
+
+    const restaurant = await this.restaurantsRepository.getById(id);
+
+    return {
+      restaurant
+    };
+  }
+}
