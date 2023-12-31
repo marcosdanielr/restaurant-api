19bc860990c233555f180a18cacadfe9f45a8408
marcosdanielr <marcosdaniel99@protonmail.com>
2023-12-31 02:36:39 -0300

    chore: connection with pg

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,9 @@
 import fastify from 'fastify';
+import fastifyPostgres from '@fastify/postgres';
+import { env } from './env';
 
 export const app = fastify();
+
+app.register(fastifyPostgres, {
+  connectionString: env.DATABASE_URL 
+});
