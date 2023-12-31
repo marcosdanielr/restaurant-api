811c74e8745d7667de49076f27c3b0aafb23454c
marcosdanielr <marcosdaniel99@protonmail.com>
2023-12-31 01:17:20 -0300

    style: eslint config

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
-import { app } from "./app";
+import { app } from './app';
 
 const PORT = 4000;
 
 app
   .listen({ port: PORT })
-  .then(() => console.log(`HTTP Server Running on Port ${PORT}`));
+  .then(() => {
+    console.log(`HTTP Server Running on Port ${PORT}`);
+  });
