5d095ec545f7bb67467b7504b253058e486aa927
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-01 12:42:55 -0300

    test: delete restaurant by id (use case)

---
+++ src/use-cases/restaurants/delete-restaurant.spec.ts
@@ -0,0 +1,34 @@
+import { InMemoryRestaurantsRepository } from "@/repositories/in-memory/in-memory-restaurants-repository";
+import { describe, it, expect, beforeEach } from "vitest";
+import { DeleteRestaurantUseCase } from "./delete-restaurant";
+
+let restaurantsRepository: InMemoryRestaurantsRepository;
+let sut: DeleteRestaurantUseCase;
+
+describe("Delete Restaurant Use Case", () => {
+  beforeEach(() => {
+    restaurantsRepository = new InMemoryRestaurantsRepository();
+    sut = new DeleteRestaurantUseCase(restaurantsRepository);
+  });
+
+  it("should be able to delete restaurant", async () => {
+
+    for (let i = 0; i < 5; i++) {
+      await restaurantsRepository.create({
+        name: `Restaurante ${i}`,
+        address: `Avenida ${i}`
+      });
+    }
+
+    const restaurantId = restaurantsRepository.restaurants[2].id;
+
+    await sut.execute({
+      id: restaurantId
+    });
+
+    const restaurant =  await restaurantsRepository.getById(restaurantId);
+
+    expect(restaurant).toEqual(null);
+    expect(restaurantsRepository.restaurants.length).toEqual(4);
+  });
+});
