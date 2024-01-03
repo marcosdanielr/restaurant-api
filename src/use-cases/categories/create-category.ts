665627763d35a9067796c36741e966a615bf6011
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-02 22:20:31 -0300

    feat: create category (use case)

---
+++ src/use-cases/categories/create-category.ts
@@ -0,0 +1,19 @@
+import { CategoriesRepository } from "@/repositories/categories-repository";
+import { CategoryInput } from "@/types/categories/categories-repository";
+
+type CreateCategoryUseCaseRequest = {
+  restaurant_id: string;
+  body: CategoryInput
+}
+
+export class CreateCategoryUseCase {
+  constructor(private categoriesRepository: CategoriesRepository) {}
+
+  async execute({
+    restaurant_id,
+    body
+  }: CreateCategoryUseCaseRequest): Promise<void> {
+
+    await this.categoriesRepository.create(restaurant_id, body);
+  }
+}
