d4002c6a2bce69a76063d556fc4977903df96c7f
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-02 21:55:36 -0300

    fix: categories-repository class name

--- src/repositories/categories-repository.ts
+++ src/repositories/categories-repository.ts
@@ -1,5 +1,5 @@
 import { CategoryInput } from "@/types/categories/categories-repository";
 
-export interface CategoryRepository {
+export interface CategoriesRepository {
   create(restaurantId: string, body: CategoryInput): Promise<void>;
 }
