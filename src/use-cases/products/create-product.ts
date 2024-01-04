dd4631889c3e8edd922ef161400b48cd20b83e58
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-04 00:52:30 -0300

    feat: create product (use case)

---
+++ src/use-cases/products/create-product.ts
@@ -0,0 +1,18 @@
+import { ProductsRepository } from "@/repositories/products-repository";
+import { CreateProductInput } from "@/types/products/products-repository";
+
+type CreateProductUseCaseRequest = {
+  restaurant_id: string;
+  body: CreateProductInput;
+};
+
+export class CreateProductUseCase {
+  constructor(private productsRepository: ProductsRepository) {}
+
+  async execute({
+    restaurant_id,
+    body,
+  }: CreateProductUseCaseRequest): Promise<void> {
+    await this.productsRepository.create(restaurant_id, body);
+  }
+}
