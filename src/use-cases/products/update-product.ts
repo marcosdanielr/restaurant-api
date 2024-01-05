0a316ee67b68f6d75afecd9b2fe2049be77c9f30
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-05 00:01:31 -0300

    feat: update product (use case)

---
+++ src/use-cases/products/update-product.ts
@@ -0,0 +1,20 @@
+import { ProductsRepository } from "@/repositories/products-repository";
+import { UpdateProductInput } from "@/types/products/products-repository";
+
+type UpdateProductUseCaseRequest = {
+  restaurant_id: string;
+  id: string;
+  body: UpdateProductInput
+};
+
+export class UpdateProductUseCase {
+  constructor(private productsRepository: ProductsRepository) {}
+
+  async execute({
+    restaurant_id,
+    id,
+    body
+  }: UpdateProductUseCaseRequest): Promise<void> {
+    await this.productsRepository.update(restaurant_id, id, body);
+  }
+}
