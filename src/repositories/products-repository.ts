36398cda4d3cb323eb61254ead22405d8faa5e14
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-04 22:08:46 -0300

    feat: list restaurant products in products repository

--- src/repositories/products-repository.ts
+++ src/repositories/products-repository.ts
@@ -1,5 +1,6 @@
-import { CreateProductInput } from "@/types/products/products-repository";
+import { CreateProductInput, Product } from "@/types/products/products-repository";
 
 export interface ProductsRepository {
   create(restaurant_id: string, body: CreateProductInput): Promise<void>;
+  listRestaurantProducts(restaurant_id: string): Promise<Product[]>;
 }
