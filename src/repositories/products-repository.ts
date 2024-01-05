f4e5e15a1953342215f5de9a29ec11fa5faa11b8
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-04 23:19:39 -0300

    feat: delete in products repository

--- src/repositories/products-repository.ts
+++ src/repositories/products-repository.ts
@@ -3,4 +3,5 @@ import { CreateProductInput, Product } from "@/types/products/products-repositor
 export interface ProductsRepository {
   create(restaurant_id: string, body: CreateProductInput): Promise<void>;
   listRestaurantProducts(restaurant_id: string): Promise<Product[]>;
+  delete(restaurant_id: string, id: string): Promise<void>;
 }
