7068586cc0e8a9b288f1d9049d1cb7edf005033d
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-04 23:57:20 -0300

    feat: update method in products repository

--- src/repositories/products-repository.ts
+++ src/repositories/products-repository.ts
@@ -4,4 +4,5 @@ export interface ProductsRepository {
   create(restaurant_id: string, body: CreateProductInput): Promise<void>;
   listRestaurantProducts(restaurant_id: string): Promise<Product[]>;
   delete(restaurant_id: string, id: string): Promise<void>;
+  update(restaurant_id: string, id: string): Promise<void>;
 }
