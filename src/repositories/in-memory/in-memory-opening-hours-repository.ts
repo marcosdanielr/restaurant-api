2993e74ede36645409a22cbf803755e9d2a31189
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-06 00:45:05 -0300

    feat: create opening hour in memory repository

---
+++ src/repositories/in-memory/in-memory-opening-hours-repository.ts
@@ -0,0 +1,25 @@
+import { OpeningHours, Weekday } from "@/types/repositories/opening-hours-repository";
+import { OpeningHoursRepository } from "../opening-hours-repository";
+
+
+export class InMemoryOpeningHoursRepository implements OpeningHoursRepository {
+  public openingHours: OpeningHours[] = []; 
+
+  async create(body: OpeningHours) {
+    const { restaurant_id, weekday, start_hour, final_hour } = body;
+
+    const openingHour = {
+      restaurant_id, 
+      weekday, 
+      start_hour, 
+      final_hour
+    };
+
+    this.openingHours.push(openingHour);
+
+  }
+
+  async getByWeekday(restaurant_id: string, weekday: Weekday) {
+    return null;
+  }
+}
