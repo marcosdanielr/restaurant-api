64b53d67bdf57c74731c1b93ed45442a6d3faaf9
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-06 00:25:33 -0300

    feat: list in opening hours repository

--- src/repositories/opening-hours-repository.ts
+++ src/repositories/opening-hours-repository.ts
@@ -2,4 +2,5 @@ import { OpeningHours } from "@/types/repositories/opening-hours-repository";
 
 export interface OpeningHoursRepository {
   create(body: OpeningHours): Promise<void>;
+  list(restaurant_id: string): Promise<OpeningHoursRepository>
 }
