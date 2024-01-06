a3c17d19736449942f96e4ac41e1292475d62249
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-06 01:27:48 -0300

    test: list opening hours (use case)

---
+++ src/use-cases/opening-hours/list-opening-hours.spec.ts
@@ -0,0 +1,61 @@
+
+import { describe, it, expect, beforeEach } from "vitest";
+import { ListOpeningHoursUseCase } from "./list-opening-hours";
+import { InMemoryOpeningHoursRepository } from "@/repositories/in-memory/in-memory-opening-hours-repository";
+import { InMemoryRestaurantsRepository } from "@/repositories/in-memory/in-memory-restaurants-repository";
+
+let restaurantsRepository: InMemoryRestaurantsRepository;
+let openingHoursRepository: InMemoryOpeningHoursRepository;
+let sut: ListOpeningHoursUseCase;
+
+describe("List Opening Hours Use Case", () => {
+  beforeEach(() => {
+    restaurantsRepository = new InMemoryRestaurantsRepository();
+    openingHoursRepository = new InMemoryOpeningHoursRepository();
+    sut = new ListOpeningHoursUseCase(openingHoursRepository);
+  });
+
+  it("should be able to list opening hours", async () => {
+
+    await restaurantsRepository.create({
+      name: "Lanchonete",
+      address: "Avenida",
+    });
+
+    const { id: restaurant_id } = restaurantsRepository.restaurants[0];
+
+    await openingHoursRepository.create({
+      restaurant_id,
+      weekday: "FRIDAY",
+      start_hour: "06:00",
+      final_hour: "12:00",
+    });
+
+
+    await openingHoursRepository.create({
+      restaurant_id,
+      weekday: "MONDAY",
+      start_hour: "06:00",
+      final_hour: "12:00",
+    });
+
+    const { openingHours } = await sut.execute({
+      restaurant_id,
+    });
+
+    expect(openingHours).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          restaurant_id,
+          weekday: "FRIDAY",
+        }),
+        expect.objectContaining({
+          restaurant_id,
+          weekday: "MONDAY",
+        })
+      ])
+    );
+
+    expect(openingHours.length).toEqual(2);
+  });
+});
