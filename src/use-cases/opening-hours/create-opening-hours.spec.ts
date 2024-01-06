1b69a078031d6c0b5713986f2cbbfb153fc10645
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-06 01:09:28 -0300

    test: shouldn't able create opening hour with invalid weekday (use-case)

--- src/use-cases/opening-hours/create-opening-hours.spec.ts
+++ src/use-cases/opening-hours/create-opening-hours.spec.ts
@@ -2,6 +2,7 @@ import { describe, it, expect, beforeEach } from "vitest";
 import { CreateOpeningHoursUseCase } from "./create-opening-hours";
 import { InMemoryOpeningHoursRepository } from "@/repositories/in-memory/in-memory-opening-hours-repository";
 import { InMemoryRestaurantsRepository } from "@/repositories/in-memory/in-memory-restaurants-repository";
+import { InvalidWeekdayError } from "../errors/invalid-weekday-error";
 
 let restaurantsRepository: InMemoryRestaurantsRepository;
 let openingHoursRepository: InMemoryOpeningHoursRepository;
@@ -32,7 +33,6 @@ describe("Create Opening Hours Use Case", () => {
 
     const openingHours = openingHoursRepository.openingHours;
 
-
     expect(openingHours[0]).toEqual(
       expect.objectContaining({
         restaurant_id,
@@ -41,5 +41,23 @@ describe("Create Opening Hours Use Case", () => {
         final_hour: "18:00"
       })
     );
+  });
+
+  it("shouldn't able to create opening hours if has invalid weekday", async () => {
+    await restaurantsRepository.create({
+      name: "Lanchonete",
+      address: "Avenida",
+    });
+
+    const { id: restaurant_id } = restaurantsRepository.restaurants[0];
+
+    await expect(() => 
+      sut.execute({
+        restaurant_id,
+        weekday: "TEST" as any,
+        start_hour: "08:10",
+        final_hour: "18:00"
+      })
+    ).rejects.toBeInstanceOf(InvalidWeekdayError);
   });
 });
