2331cd8adec03c4eee97d5de5a8a3747b6188502
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-06 02:17:53 -0300

    chore: minimum interval time on create opening hour (use case)

--- src/use-cases/opening-hours/create-opening-hours.ts
+++ src/use-cases/opening-hours/create-opening-hours.ts
@@ -5,6 +5,8 @@ import { WeekdaysEnum } from "@/constants/weekdays-enum";
 import { WeekdayAlreadyExistsError } from "../errors/weekday-already-exists-error";
 import { validateTimeFormat } from "@/utils/validate-hour-format";
 import { InvalidTimeFormatError } from "../errors/invalid-time-format-error";
+import { isMinimumIntervalFifteenMinutes } from "@/utils/calc-minutes-difference";
+import { MinimumIntervalTimeError } from "../errors/minimum-interval-time-error";
 
 export class CreateOpeningHoursUseCase {
   constructor(private openingHoursRepository: OpeningHoursRepository) {}
@@ -20,6 +22,10 @@ export class CreateOpeningHoursUseCase {
 
     if (isNotValidHour) {
       throw new InvalidTimeFormatError();
+    }
+
+    if (!isMinimumIntervalFifteenMinutes(start_time, end_time)) {
+      throw new MinimumIntervalTimeError();
     }
 
     const isValidWeekDay = WeekdaysEnum[weekday];
