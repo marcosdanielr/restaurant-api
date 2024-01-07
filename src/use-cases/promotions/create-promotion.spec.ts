328838806bd3a6baa40ca147514d02a37344d84e
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-07 20:51:23 -0300

    test: verify time format on create promotion

--- src/use-cases/promotions/create-promotion.spec.ts
+++ src/use-cases/promotions/create-promotion.spec.ts
@@ -1,13 +1,10 @@
 import { describe, it, expect, beforeEach } from "vitest";
 import { InMemoryIRestaurantsRepository } from "@/repositories/in-memory/in-memory-restaurants-repository";
-import { InvalidWeekdayError } from "../errors/invalid-weekday-error";
-import { WeekdayAlreadyExistsError } from "../errors/weekday-already-exists-error";
-import { InvalidTimeFormatError } from "../errors/invalid-time-format-error";
-import { MinimumIntervalTimeError } from "../errors/minimum-interval-time-error";
 import { InMemoryIPromotionsRepository } from "@/repositories/in-memory/in-memory-promotions-repository";
 import { CreatePromotionUseCase } from "./create-promotion";
 import { InMemoryIProductsRepository } from "@/repositories/in-memory/in-memory-products-repository";
 import { InMemoryICategoriesRepository } from "@/repositories/in-memory/in-memory-categories-repository";
+import { InvalidTimeFormatError } from "../errors/invalid-time-format-error";
 
 let restaurantsRepository: InMemoryIRestaurantsRepository;
 let categoriesRepository: InMemoryICategoriesRepository;
@@ -70,4 +67,40 @@ describe("Create Promotion Use Case", () => {
     );
   });
 
+
+  it("shouldn't able to create promotion if has invalid time format", async () => {
+
+    await restaurantsRepository.create({
+      name: "Lanchonete",
+      address: "Avenida",
+    });
+
+    const { id: restaurant_id } = restaurantsRepository.restaurants[0];
+
+    await categoriesRepository.create(restaurant_id, {
+      name: "Bebidas"
+    });
+
+    const { id: category_id} = categoriesRepository.categories[0];
+
+    await productsRepository.create(restaurant_id, {
+      name: "Monster",
+      price: 9.50,
+      category_id
+    });
+
+    const { id: product_id } = productsRepository.products[0];
+
+    await expect(() => 
+      sut.execute({
+        product_id, 
+        price: 7.50,
+        description: "Promoção!",
+        weekday: "SUNDAY",
+        start_time: "08h10",
+        end_time: "18:00"
+      })
+    ).rejects.toBeInstanceOf(InvalidTimeFormatError);
+    
+  });
 });
