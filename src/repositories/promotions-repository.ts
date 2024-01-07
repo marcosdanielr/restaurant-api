0427628811098034076854c5fd3984e63b5b9118
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-07 16:23:59 -0300

    feat: get by weekday in promotions repository interface

--- src/repositories/promotions-repository.ts
+++ src/repositories/promotions-repository.ts
@@ -1,5 +1,7 @@
 import { Promotion } from "@/models/promotions-model";
+import { Weekday } from "@/types/weekday";
 
 export interface IPromotionsRepository {
   create(body: Promotion): Promise<void>;
+  getByWeekday(product_id: string, weekday: Weekday): Promise<Promotion | null>
 }
