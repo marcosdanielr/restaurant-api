d5f1f150a000e0ae2416cf0982ce69f39a7b4e3a
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-07 20:32:48 -0300

    feat: in memory promotions repository

---
+++ src/repositories/in-memory/in-memory-promotions-repository.ts
@@ -0,0 +1,28 @@
+import { Promotion } from "@/models/promotions-model";
+import { IPromotionsRepository } from "../promotions-repository";
+import { Weekday } from "@/types/weekday";
+
+export class InMemoryIPromotionsRepository implements IPromotionsRepository {
+  public promotions: Promotion[] = [];
+
+  async create(body: Promotion) {
+    const { product_id, price, description, weekday, start_time, end_time } = body;
+
+    const promotion = {
+      product_id,
+      price,
+      description,
+      weekday,
+      start_time,
+      end_time
+    };
+
+    this.promotions.push(promotion);
+  }
+
+  async getByWeekday(product_id: string, weekday: Weekday) {
+    const promotion = this.promotions.find(promotion => promotion.product_id === product_id && promotion.weekday === weekday) ?? null;
+
+    return promotion; 
+  }
+}
