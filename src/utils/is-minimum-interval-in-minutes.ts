1697eef0f6f121941ed9040e7a8194e980ac95fa
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-07 02:34:10 -0300

    refactor: is minimum interval in minutes util

--- src/utils/is-minimum-interval-in-minutes.ts
+++ src/utils/is-minimum-interval-in-minutes.ts
@@ -1,11 +1,9 @@
-export function isMinimumIntervalFifteenMinutes(start_time: string, end_time: string) {
- 
-  const MINIMUM_INTERVAL = 15;
+export function isMinimumIntervalInMinutes(start_time: string, end_time: string, minimum_interval: number) {
 
   const startDate = new Date(0, 0, 0, ...start_time.split(":").map(Number));
   const endDate = new Date(0, 0, 0, ...end_time.split(":").map(Number));
 
   const minutesDifference = Math.floor((endDate.getTime() - startDate.getTime()) / (1000 * 60));
 
-  return minutesDifference >= MINIMUM_INTERVAL;
+  return minutesDifference >= minimum_interval;
 }
