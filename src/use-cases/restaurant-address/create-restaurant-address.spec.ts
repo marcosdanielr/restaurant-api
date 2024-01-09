51d169e2032bd7e0e4cc1898ecc63fb0809995b2
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-09 02:41:39 -0300

    test: create restaurant address (use case)

---
+++ src/use-cases/restaurant-address/create-restaurant-address.spec.ts
@@ -0,0 +1,37 @@
+import { describe, it, expect, beforeEach } from "vitest";
+import { InMemoryRestaurantAddressRepository } from "@/repositories/in-memory/in-memory-restaurant-address-repository";
+import { CreateRestaurantAddressUseCase } from "./create-restaurant-address";
+import { InMemoryRestaurantsRepository } from "@/repositories/in-memory/in-memory-restaurants-repository";
+
+let restaurantsRepository: InMemoryRestaurantsRepository;
+let restaurantAddressRepository: InMemoryRestaurantAddressRepository;
+let sut: CreateRestaurantAddressUseCase;
+
+describe("Create Restaurant Address Use Case", () => {
+  beforeEach(() => {
+    restaurantsRepository = new InMemoryRestaurantsRepository();
+    restaurantAddressRepository = new InMemoryRestaurantAddressRepository();
+    sut = new CreateRestaurantAddressUseCase(restaurantAddressRepository);
+  });
+
+  it("should be able to create restaurant address", async () => {
+    await restaurantsRepository.create({
+      name: "Frituras"
+    });
+
+    const { id: restaurant_id } = restaurantsRepository.restaurants[0];
+
+
+    await sut.execute({
+      restaurant_id,
+      street: "Rua tal",
+      district: "Algum bairro aí",
+      number: 231,
+      zip_code: "21223"
+    });
+
+    const restaurantAddress = restaurantAddressRepository.restaurantsAdresses;
+
+    expect(restaurantAddress.length).toEqual(1);
+  });
+});
