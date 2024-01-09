c5259937a6da51b15158db3aaf5af7959877d0b4
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-09 00:48:37 -0300

    fix: create in promotions repository interface

--- src/repositories/promotions-repository.ts
+++ src/repositories/promotions-repository.ts
@@ -1,7 +1,7 @@
-import { Promotion } from "@/models/promotions-model";
+import { CreatePromotionRequest, Promotion } from "@/models/promotions-model";
 import { Weekday } from "@/types/weekday";
 
 export interface IPromotionsRepository {
-  create(body: Promotion): Promise<void>;
+  create(body: CreatePromotionRequest): Promise<void>;
   getByWeekday(product_id: string, weekday: Weekday): Promise<Promotion | null>;
 }
