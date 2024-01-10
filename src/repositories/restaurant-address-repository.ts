6cb6f5f6566312a2bce449422dea6c2fa7763401
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-10 00:47:25 -0300

    feat: get in restaurant address repository interface

--- src/repositories/restaurant-address-repository.ts
+++ src/repositories/restaurant-address-repository.ts
@@ -2,4 +2,5 @@ import { RestaurantAddress } from "@/models/restaurant-address-model";
 
 export interface IRestaurantAddressRepository {
   create(body: RestaurantAddress): Promise<void>;
+  getByRestaurantId(restaurantId: string): Promise<RestaurantAddress | null>;
 }
