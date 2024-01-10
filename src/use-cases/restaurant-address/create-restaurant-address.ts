75e68d70abf50a9b5c600dbfeca23a179410a8bb
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-10 01:22:20 -0300

    chore: verify if restaurant address exists on create

--- src/use-cases/restaurant-address/create-restaurant-address.ts
+++ src/use-cases/restaurant-address/create-restaurant-address.ts
@@ -1,5 +1,6 @@
 import { RestaurantAddress } from "@/models/restaurant-address-model";
 import { IRestaurantAddressRepository } from "@/repositories/restaurant-address-repository";
+import { RestaurantAddressAlreadyExistsError } from "../errors/restaurant-address-already-exists-error";
 
 export class CreateRestaurantAddressUseCase {
   constructor(private restaurantAdressRepository: IRestaurantAddressRepository) {}
@@ -13,6 +14,12 @@ export class CreateRestaurantAddressUseCase {
     number,
     zip_code
   }: RestaurantAddress): Promise<void> {
+
+    const restaurantAddressExists = await this.restaurantAdressRepository.getByRestaurantId(restaurant_id);
+
+    if (restaurantAddressExists) {
+      throw new RestaurantAddressAlreadyExistsError();
+    }
 
     await this.restaurantAdressRepository.create({
       restaurant_id,
