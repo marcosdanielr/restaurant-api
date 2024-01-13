2e9ecb80535e59a443f0710a13f626347e79d14f
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-13 02:31:00 -0300

    feat: list restaurants

--- src/http/controllers/restaurants/routes.ts
+++ src/http/controllers/restaurants/routes.ts
@@ -3,10 +3,12 @@ import { FastifyInstance } from "fastify";
 import { create } from "./create";
 import { deleteById } from "./delete";
 import { getById } from "./get";
+import { list } from "./list";
 
 
 export async function restaurantsRoutes(app: FastifyInstance) {
   app.post("/restaurants", create);
   app.delete("/restaurants/:id", deleteById);
   app.get("/restaurants/:id", getById);
+  app.get("/restaurants", list);
 }
