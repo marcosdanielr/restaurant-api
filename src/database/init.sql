971b909818bb446bc70023f8e919c2c02a280ed3
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-03-27 02:38:32 -0300

    feat: products table

--- src/database/init.sql
+++ src/database/init.sql
@@ -8,3 +8,15 @@ CREATE TABLE IF NOT EXISTS "restaurants" (
   "updated_at" TIMESTAMP DEFAULT NOW() NOT NULL,
   CONSTRAINT pk_restaurants PRIMARY KEY (id)
 );
+
+
+CREATE TABLE IF NOT EXISTS "products" (
+  "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+  "restaurant_id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+  "name" VARCHAR(255) NOT NULL,
+  "image_path" VARCHAR(255),
+  "price" DECIMAL NOT NULL,
+  "created_at" TIMESTAMP DEFAULT NOW() NOT NULL,
+  "updated_at" TIMESTAMP DEFAULT NOW() NOT NULL,
+  CONSTRAINT pk_products PRIMARY KEY (id)
+);
