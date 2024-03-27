94d08ce52ed4fd1ce7497439a844359d52e7ff2c
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-03-27 04:08:04 -0300

    feat: create product http
    add create product controller, route and swagger

--- src/http/controllers/products/create-product.ts
+++ src/http/controllers/products/create-product.ts
@@ -5,14 +5,15 @@ import { z } from "zod";
 
 export async function createProduct(request: FastifyRequest, reply: FastifyReply) {
   const createProductBodySchema = z.object({
+    restaurant_id: z.string(),
     name: z.string(),
     image_path: z.string().optional(),
     price: z.number(),
     category_id: z.string()
   });
 
-
   const { 
+    restaurant_id,
     name, 
     image_path, 
     price, 
@@ -22,7 +23,7 @@ export async function createProduct(request: FastifyRequest, reply: FastifyReply
   const createProductUseCase = makeCreateProductUseCase();
 
   await createProductUseCase.execute({
-    restaurant_id: "ss",
+    restaurant_id,
     body: {
       name,
       image_path: image_path ?? "",
--- src/repositories/postgresql/postgresql-products-repository.ts
+++ src/repositories/postgresql/postgresql-products-repository.ts
@@ -1,16 +1,16 @@
 import { app } from "@/app";
 import { IProductsRepository } from "../products-repository";
-import { CreateProductRequest, Product, UpdateProductRequest } from "@/models/products-model";
+import { CreateProductRequest, UpdateProductRequest } from "@/models/products-model";
 
 export class PostgreSQLProductsRepository implements IProductsRepository {
 
   async create(restaurant_id_req: string, body: CreateProductRequest) {
     const restaurant_id = restaurant_id_req;
-    const {name, image_path, price, category_id } = body;
+    const { name, image_path, price, category_id } = body;
 
     await app.pg.query(
-      "INSERT INTO restaurants (name, image_path, price, category_id, restaurant_id) VALUES ($1, $2, $3, $4) RETURNING *",
-      [name, image_path, price, category_id, restaurant_id]
+      "INSERT INTO products (name, image_path, price, restaurant_id) VALUES ($1, $2, $3, $4) RETURNING *",
+      [name, image_path, price, restaurant_id]
     );
   }
 
