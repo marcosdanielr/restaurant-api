a9c9ac01d82eac952e07c68bcf8d305f9bcd0847
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-03-28 02:10:40 -0300

    feat: list categories repository interface

--- src/repositories/categories-repository.ts
+++ src/repositories/categories-repository.ts
@@ -1,5 +1,6 @@
-import { CategoryRequest } from "@/models/categories-model";
+import { Category, CategoryRequest } from "@/models/categories-model";
 
 export interface ICategoriesRepository {
   create(restaurant_id: string, body: CategoryRequest): Promise<void>;
+  listCategories(restaurant_id: string): Promise<Category[]>;
 }
