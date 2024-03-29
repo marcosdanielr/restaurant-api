b2958369b6c03a596eeb4b41145c13e79b4006bb
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-03-29 02:37:05 -0300

    chore: error if restaurant not exists in controller

--- src/http/controllers/restaurants/get-restaurant-by-id.ts
+++ src/http/controllers/restaurants/get-restaurant-by-id.ts
@@ -1,4 +1,5 @@
 import { StatusCodes } from "@/constants/status-codes-enum";
+import { RestaurantNotFoundError } from "@/use-cases/errors/restaurant-not-found-error";
 import { makeGetRestaurantUseCase } from "@/use-cases/factories/restaurants/make-get-restaurant-use-case";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
@@ -6,16 +7,27 @@ import { z } from "zod";
 export async function getRestaurantById(request: FastifyRequest, reply: FastifyReply) {
 
   const getRestaurantBodySchema = z.object({
-    id: z.string(),
+    id: z.string().uuid(),
   });
 
   const { id } = getRestaurantBodySchema.parse(request.params);
 
   const getRestaurantUseCase = makeGetRestaurantUseCase();
 
-  const restaurant = await getRestaurantUseCase.execute({
-    id
-  });
+  try {
+    const restaurant = await getRestaurantUseCase.execute({
+      id
+    });
 
-  return reply.status(StatusCodes.OK).send(restaurant);
+    return reply.status(StatusCodes.OK).send(restaurant);
+
+  } catch (error) {
+    if (error instanceof RestaurantNotFoundError) {
+      return reply.status(StatusCodes.NOT_FOUND).send({
+        message: error.message
+      });
+    }
+
+    return reply.status(StatusCodes.INTERNAL_SERVER_ERROR).send();
+  }
 }
