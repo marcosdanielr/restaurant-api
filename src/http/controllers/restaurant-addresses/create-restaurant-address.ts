589509ec954224eb0b897c426a865e80c13a4341
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-03-29 08:31:54 -0300

    fix: get restaurant controller errors

--- src/http/controllers/restaurant-addresses/create-restaurant-address.ts
+++ src/http/controllers/restaurant-addresses/create-restaurant-address.ts
@@ -1,4 +1,5 @@
 import { StatusCodes } from "@/constants/status-codes-enum";
+import { RestaurantAddressAlreadyExistsError } from "@/use-cases/errors/restaurant-address-already-exists-error";
 import { RestaurantNotFoundError } from "@/use-cases/errors/restaurant-not-found-error";
 import { makeCreateRestaurantAddressUseCase } from "@/use-cases/factories/restaurant-addresses/make-create-restaurant-address-use-case";
 import { FastifyReply, FastifyRequest } from "fastify";
@@ -38,7 +39,15 @@ export async function createRestaurantAddress(request: FastifyRequest, reply: Fa
     return reply.status(StatusCodes.CREATED).send();
   } catch (error) {
     if (error instanceof RestaurantNotFoundError) {
-      return reply.status(StatusCodes.NOT_FOUND).send(error.message);
+      return reply.status(StatusCodes.NOT_FOUND).send({
+        message: error.message 
+      });
+    }
+
+    if (error instanceof RestaurantAddressAlreadyExistsError) {
+      return reply.status(StatusCodes.NOT_FOUND).send({
+        message: error.message
+      });
     }
 
     return reply.status(StatusCodes.INTERNAL_SERVER_ERROR).send();
