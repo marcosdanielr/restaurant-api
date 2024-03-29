6933ef5c6d4a06faf8f3d45bc43c3d166125f4c0
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-03-29 02:28:41 -0300

    chore: verify if restaurant exists to update

--- src/http/controllers/restaurants/update-restaurant-by-id.ts
+++ src/http/controllers/restaurants/update-restaurant-by-id.ts
@@ -1,11 +1,12 @@
 import { StatusCodes } from "@/constants/status-codes-enum";
+import { RestaurantNotFoundError } from "@/use-cases/errors/restaurant-not-found-error";
 import { makeUpdateRestaurantUseCase } from "@/use-cases/factories/restaurants/make-update-restaurant-use-case";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 
 export async function updateRestaurantById(request: FastifyRequest, reply: FastifyReply) {
   const updateRestaurantParamsSchema = z.object({
-    id: z.string(),
+    id: z.string().uuid(),
   });
 
   const updateRestaurantBodySchema = z.object({
@@ -16,15 +17,25 @@ export async function updateRestaurantById(request: FastifyRequest, reply: Fasti
   const { id } = updateRestaurantParamsSchema.parse(request.params);
   const { name, image_path } = updateRestaurantBodySchema.parse(request.body);
 
-  const updateRestaurantUseCase = makeUpdateRestaurantUseCase();
+  try {
+    const updateRestaurantUseCase = makeUpdateRestaurantUseCase();
 
-  await updateRestaurantUseCase.execute({
-    id,
-    body: {
-      name,
-      image_path
-    } 
-  });
+    await updateRestaurantUseCase.execute({
+      id,
+      body: {
+        name,
+        image_path
+      } 
+    });
 
-  return reply.status(StatusCodes.OK).send();
+    return reply.status(StatusCodes.OK).send();
+  } catch (error) {
+    if (error instanceof RestaurantNotFoundError) {
+      return reply.status(StatusCodes.NOT_FOUND).send({
+        message: error.message
+      });
+    }
+
+    return reply.status(StatusCodes.INTERNAL_SERVER_ERROR).send();
+  }
 }
