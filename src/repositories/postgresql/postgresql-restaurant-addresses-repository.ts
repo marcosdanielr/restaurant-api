16b936aaec1e337fe61d2b4c97e848e10b3e0f86
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-03-29 06:58:21 -0300

    feat: create restaurant address sql query

---
+++ src/repositories/postgresql/postgresql-restaurant-addresses-repository.ts
@@ -0,0 +1,27 @@
+import { app } from "@/app";
+import { IRestaurantAddressesRepository } from "../restaurant-addresses-repository";
+import { CreateRestaurantAddressRequest } from "@/models/restaurant-addresses-model";
+
+export class PostgreSQLRestaurantAddressesRepository implements IRestaurantAddressesRepository {
+
+  async create(body: CreateRestaurantAddressRequest) {
+    const { 
+      restaurant_id,
+      city,
+      state,
+      zip_code,
+      street,
+      district,
+      number
+    } = body;
+
+    await app.pg.query(
+      "INSERT INTO restaurant_addresses (restaurant_id, city, state, zip_code, street, district, number) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+      [restaurant_id, city, state, zip_code, street, district, number]
+    );
+  }
+
+  async getByRestaurantId(restaurantId: string) {
+    return null; 
+  }
+}
