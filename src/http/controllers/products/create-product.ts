1ba678d99282fd84dd2a163a1d7237dcdf953c7c
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-03-29 02:22:10 -0300

    chore: verify if restaurant exists to create a product

--- src/http/controllers/products/create-product.ts
+++ src/http/controllers/products/create-product.ts
@@ -1,4 +1,5 @@
 import { StatusCodes } from "@/constants/status-codes-enum";
+import { RestaurantNotFoundError } from "@/use-cases/errors/restaurant-not-found-error";
 import { makeCreateProductUseCase } from "@/use-cases/factories/products/make-create-product-use-case";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
@@ -22,15 +23,26 @@ export async function createProduct(request: FastifyRequest, reply: FastifyReply
 
   const createProductUseCase = makeCreateProductUseCase();
 
-  await createProductUseCase.execute({
-    restaurant_id,
-    body: {
-      name,
-      image_path: image_path ?? "",
-      price,
-      category_id
-    }
-  });
+  try {
+    await createProductUseCase.execute({
+      restaurant_id,
+      body: {
+        name,
+        image_path: image_path ?? "",
+        price,
+        category_id
+      }
+    });
 
-  return reply.status(StatusCodes.CREATED).send();
+    return reply.status(StatusCodes.CREATED).send();
+  } catch (error) {
+    if (error instanceof RestaurantNotFoundError) {
+      return reply.status(StatusCodes.NOT_FOUND).send({
+        message: error.message
+      });
+    }
+
+    return reply.status(StatusCodes.INTERNAL_SERVER_ERROR).send();
+  }
+
 }
