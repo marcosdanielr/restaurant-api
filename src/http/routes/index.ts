c2aa509eeaf6d3838a8855dccbd7707c18a208fa
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-03-29 07:19:57 -0300

    feat: create restaurant address route

--- src/http/routes/index.ts
+++ src/http/routes/index.ts
@@ -3,9 +3,11 @@ import { FastifyInstance } from "fastify";
 import { restaurantsRoutes } from "./restaurants-routes";
 import { productsRoutes } from "./products-routes";
 import { categoriesRoutes } from "./categories-routes";
+import { restaurantAddressesRoutes } from "./restaurant-addresses-routes";
 
 export async function routes(app: FastifyInstance) {
   restaurantsRoutes(app);
-  productsRoutes(app);
+  restaurantAddressesRoutes(app);
   categoriesRoutes(app);
+  productsRoutes(app);
 }
---
+++ src/http/routes/restaurant-addresses-routes.ts
@@ -0,0 +1,23 @@
+import { FastifyInstance } from "fastify";
+import { createRestaurantAddress } from "../controllers/restaurant-addresses/create-restaurant-address";
+
+export async function restaurantAddressesRoutes(app: FastifyInstance) {
+  app.post("/restaurant-addresses",   {
+    schema: {
+      tags: ["Restaurant Addresses"],
+      body: {
+        type: "object",
+        properties: {
+          restaurant_id: { type: "string" },
+          city: { type: "string" },
+          state: { type: "string" },
+          zip_code: { type: "string" },
+          district: { type: "string" },
+          street: { type: "string" },
+          number: { type: "string" }
+        }
+      }
+    }
+  },
+  createRestaurantAddress);
+}
