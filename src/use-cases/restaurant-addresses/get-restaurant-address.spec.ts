697d84b079b5c3cfd25493f1121ec1f16fc1955b
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-03-29 07:35:37 -0300

    test: verify if restaurant exists to get address (use case)

--- src/use-cases/restaurant-addresses/get-restaurant-address.spec.ts
+++ src/use-cases/restaurant-addresses/get-restaurant-address.spec.ts
@@ -2,6 +2,7 @@ import { describe, it, expect, beforeEach } from "vitest";
 import { InMemoryRestaurantsRepository } from "@/repositories/in-memory/in-memory-restaurants-repository";
 import { GetRestaurantAddressUseCase } from "./get-restaurant-address";
 import { InMemoryRestaurantAddressesRepository } from "@/repositories/in-memory/in-memory-restaurant-addresses-repository";
+import { RestaurantNotFoundError } from "../errors/restaurant-not-found-error";
 
 let restaurantsRepository: InMemoryRestaurantsRepository;
 let restaurantAddressRepository: InMemoryRestaurantAddressesRepository;
@@ -11,7 +12,7 @@ describe("Get Restaurant Address Use Case", () => {
   beforeEach(() => {
     restaurantsRepository = new InMemoryRestaurantsRepository();
     restaurantAddressRepository = new InMemoryRestaurantAddressesRepository();
-    sut = new GetRestaurantAddressUseCase(restaurantAddressRepository);
+    sut = new GetRestaurantAddressUseCase(restaurantAddressRepository, restaurantsRepository);
   });
 
   it("should be able to get restaurant address", async () => {
@@ -48,5 +49,36 @@ describe("Get Restaurant Address Use Case", () => {
         created_at: expect.any(Date)
       })
     );
+  });
+
+  it("shouldn't be able to get address if restaurant not exists", async () => {
+    await restaurantsRepository.create({
+      name: "Frituras"
+    });
+
+    const { id: restaurant_id } = restaurantsRepository.restaurants[0];
+
+    await restaurantAddressRepository.create({
+      restaurant_id,
+      city: "São Paulo",
+      state: "SP",
+      street: "Rua tal",
+      district: "Algum bairro aí",
+      number: "231",
+    });
+
+    await restaurantAddressRepository.create({
+      restaurant_id,
+      city: "São Paulo",
+      state: "SP",
+      street: "Rua tal",
+      district: "Algum bairro aí",
+      number: "231",
+    });
+
+    await expect(() => 
+      sut.execute({ restaurant_id })
+    ).rejects.toBeInstanceOf(RestaurantNotFoundError);
+
   });
 });
