a9758dadbf53243cf5de5c68e61cb017bb7bacb9
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-03-29 01:48:33 -0300

    chore: verify if restaurant exists to create a product

--- src/use-cases/products/create-product.ts
+++ src/use-cases/products/create-product.ts
@@ -1,5 +1,7 @@
 import { CreateProductRequest } from "@/models/products-model";
 import { IProductsRepository } from "@/repositories/products-repository";
+import { IRestaurantsRepository } from "@/repositories/restaurants-repository";
+import { RestaurantNotFoundError } from "../errors/restaurant-not-found-error";
 
 type CreateProductUseCaseRequest = {
   restaurant_id: string;
@@ -7,12 +9,22 @@ type CreateProductUseCaseRequest = {
 };
 
 export class CreateProductUseCase {
-  constructor(private productsRepository: IProductsRepository) {}
+  constructor(
+    private productsRepository: IProductsRepository,
+    private restaurantRepository: IRestaurantsRepository
+  ) {}
 
   async execute({
     restaurant_id,
     body,
   }: CreateProductUseCaseRequest): Promise<void> {
+
+    const restaurantExists = await this.restaurantRepository.getById(restaurant_id);
+
+    if (!restaurantExists) {
+      throw new RestaurantNotFoundError();
+    }
+
     await this.productsRepository.create(restaurant_id, body);
   }
 }
