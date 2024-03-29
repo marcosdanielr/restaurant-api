93bce182ae8080880167004883619ec7468ef403
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-03-29 02:26:20 -0300

    chore: verify if restaurant exists to delete

--- src/http/controllers/restaurants/delete-restaurant-by-id.ts
+++ src/http/controllers/restaurants/delete-restaurant-by-id.ts
@@ -1,4 +1,5 @@
 import { StatusCodes } from "@/constants/status-codes-enum";
+import { RestaurantNotFoundError } from "@/use-cases/errors/restaurant-not-found-error";
 import { makeDeleteRestaurantUseCase } from "@/use-cases/factories/restaurants/make-delete-restaurant-use-case";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
@@ -6,16 +7,27 @@ import { z } from "zod";
 export async function deleteRestaurantById(request: FastifyRequest, reply: FastifyReply) {
 
   const deleteRestaurantBodySchema = z.object({
-    id: z.string(),
+    id: z.string().uuid(),
   });
 
   const { id } = deleteRestaurantBodySchema.parse(request.params);
 
   const deleteRestaurantUseCase = makeDeleteRestaurantUseCase();
 
-  await deleteRestaurantUseCase.execute({
-    id
-  });
+  try {
+    await deleteRestaurantUseCase.execute({
+      id
+    });
 
-  return reply.status(StatusCodes.OK).send();
+    return reply.status(StatusCodes.OK).send();
+
+  } catch (error) {
+    if (error instanceof RestaurantNotFoundError) {
+      return reply.status(StatusCodes.NOT_FOUND).send({
+        message: error.message
+      });
+    }
+
+    return reply.status(StatusCodes.INTERNAL_SERVER_ERROR).send();
+  }
 }
