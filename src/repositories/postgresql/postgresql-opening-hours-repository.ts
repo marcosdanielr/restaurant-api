22addd99628d6867eb95dfc90029b8a521cd20cd
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-03-29 16:44:05 -0300

    feat: get opening hour by weekday sql query

--- src/repositories/postgresql/postgresql-opening-hours-repository.ts
+++ src/repositories/postgresql/postgresql-opening-hours-repository.ts
@@ -9,7 +9,7 @@ export class PostgreSQLOpeningHoursRepository implements IOpeningHoursRepository
     const { restaurant_id, weekday, start_time, end_time } = body;
 
     await app.pg.query(
-      "INSERT INTO opening_hours (restaurant_id, weekday, start_time, end_time) VALUES ($1, $2) RETURNING *",
+      "INSERT INTO opening_hours (restaurant_id, weekday, start_time, end_time) VALUES ($1, $2, $3, $4) RETURNING *",
       [restaurant_id, weekday, start_time, end_time]
     );
 
@@ -19,8 +19,18 @@ export class PostgreSQLOpeningHoursRepository implements IOpeningHoursRepository
     return []; 
   }
 
-  async getByWeekday(restaurant_id_req: string, weekday: Weekday) {
-    return null;
+  async getByWeekday(restaurant_id_req: string, weekday_req: Weekday) {
+    const restaurant_id = restaurant_id_req;
+    const weekday = weekday_req;
+  
+    const { rows } = await app.pg.query(
+      "SELECT * FROM opening_hours WHERE restaurant_id = $1 AND weekday = $2 LIMIT 1",
+      [restaurant_id, weekday]
+    );
+
+    const [ opening_hour ] = rows;
+
+    return opening_hour || null;
   }
 
 }
