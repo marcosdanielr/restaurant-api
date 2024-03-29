bfd0dac71c68f16ea3b255f3964edf9150173fa5
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-03-29 06:56:55 -0300

    chore: verify if restaurant exists to create address

--- src/use-cases/restaurant-addresses/create-restaurant-address.ts
+++ src/use-cases/restaurant-addresses/create-restaurant-address.ts
@@ -1,9 +1,14 @@
 import { CreateRestaurantAddressRequest } from "@/models/restaurant-addresses-model";
 import { RestaurantAddressAlreadyExistsError } from "../errors/restaurant-address-already-exists-error";
 import { IRestaurantAddressesRepository } from "@/repositories/restaurant-addresses-repository";
+import { IRestaurantsRepository } from "@/repositories/restaurants-repository";
+import { RestaurantNotFoundError } from "../errors/restaurant-not-found-error";
 
 export class CreateRestaurantAddressUseCase {
-  constructor(private restaurantAddressesRepository: IRestaurantAddressesRepository) {}
+  constructor(
+    private restaurantAddressesRepository: IRestaurantAddressesRepository,
+    private restaurantRepository: IRestaurantsRepository
+  ) {}
 
   async execute({
     restaurant_id,
@@ -14,6 +19,12 @@ export class CreateRestaurantAddressUseCase {
     number,
     zip_code
   }: CreateRestaurantAddressRequest): Promise<void> {
+
+    const restaurantExists = await this.restaurantRepository.getById(restaurant_id);
+
+    if (!restaurantExists) {
+      throw new RestaurantNotFoundError();
+    }
 
     const restaurantAddressExists = await this.restaurantAddressesRepository.getByRestaurantId(restaurant_id);
 
