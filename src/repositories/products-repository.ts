c048cbdba6d894508f3c9965e74ae685780aa022
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-03-30 02:26:45 -0300

    feat: make create promotion use case

--- src/repositories/products-repository.ts
+++ src/repositories/products-repository.ts
@@ -2,6 +2,7 @@ import { CreateProductRequest, Product, UpdateProductRequest } from "@/models/pr
 
 export interface IProductsRepository {
   create(restaurant_id: string, body: CreateProductRequest): Promise<void>;
+  getById(id: string): Promise<Product | null>;
   listByRestaurantId(restaurant_id: string): Promise<Product[]>;
   delete(restaurant_id: string, id: string): Promise<void>;
   update(restaurant_id: string, id: string, body: UpdateProductRequest): Promise<void>;
