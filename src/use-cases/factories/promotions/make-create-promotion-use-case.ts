8850bf1bb6105c023f339453bf186ce4d76a2620
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-03-30 22:28:14 -0300

    chore: insert products repository into make create promotion use case

--- src/use-cases/factories/promotions/make-create-promotion-use-case.ts
+++ src/use-cases/factories/promotions/make-create-promotion-use-case.ts
@@ -1,10 +1,12 @@
+import { PostgreSQLProductsRepository } from "@/repositories/postgresql/postgresql-products-repository";
 import { PostgreSQLPromotionsRepository } from "@/repositories/postgresql/postgresql-promotions-repository";
 import { CreatePromotionUseCase } from "@/use-cases/promotions/create-promotion";
 
 export function makeCreatePromotionUseCase() {
   const promotionsRepository = new PostgreSQLPromotionsRepository();
+  const productsRepository = new PostgreSQLProductsRepository();
 
-  const useCase = new CreatePromotionUseCase(promotionsRepository);
+  const useCase = new CreatePromotionUseCase(promotionsRepository, productsRepository);
 
   return useCase;
 }
