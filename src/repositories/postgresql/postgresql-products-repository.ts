a37eda508c47bb54d83abe94786df6c24749958b
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-03-31 22:20:27 -0300

    chore: return promotions in products list

--- src/repositories/postgresql/postgresql-products-repository.ts
+++ src/repositories/postgresql/postgresql-products-repository.ts
@@ -26,12 +26,30 @@ export class PostgreSQLProductsRepository implements IProductsRepository {
     const restaurant_id = restaurant_id_req;
 
     const { rows: products } = await app.pg.query(
-      "SELECT * FROM products WHERE restaurant_id = $1",
+      `
+    SELECT p.*, 
+           CASE
+             WHEN COUNT(pr.id) > 0 THEN
+               COALESCE(json_agg(
+                 json_build_object(
+                   'description', pr.description,
+                   'price', pr.price,
+                   'weekday', pr.weekday,
+                   'start_time', pr.start_time,
+                   'end_time', pr.end_time
+                 )
+               ), '[]'::json)
+             ELSE '[]'::json
+           END AS promotions
+    FROM products p
+    LEFT JOIN promotions pr ON p.id = pr.product_id
+    WHERE p.restaurant_id = $1
+    GROUP BY p.id, p.restaurant_id, p.category_id, p.name, p.image_path, p.price, p.created_at, p.updated_at
+    `,
       [restaurant_id]
     );
 
-
-    return products; 
+    return products;
   }
 
   async getById(id_req: string) {
@@ -45,6 +63,5 @@ export class PostgreSQLProductsRepository implements IProductsRepository {
     const [product] = rows;
 
     return product || null; 
-      
   }
 }
