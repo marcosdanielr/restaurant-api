384c0473e273065dbc513941c1ff45b7313f4b3e
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-03-31 21:27:50 -0300

    chore: return address and opening hours when get restaurant by id

--- src/repositories/postgresql/postgresql-restaurants-repository.ts
+++ src/repositories/postgresql/postgresql-restaurants-repository.ts
@@ -44,10 +44,37 @@ export class PostgreSQLRestaurantsRepository implements IRestaurantsRepository {
     }
 
     const { rows: restaurants } = await app.pg.query(
-      "SELECT * FROM restaurants WHERE id = $1 LIMIT 1",
+      `
+    SELECT r.*, 
+      CASE 
+          WHEN ra.id IS NOT NULL THEN json_build_object(
+              'city', ra.city,
+              'state', ra.state,
+              'street', ra.street,
+              'district', ra.district,
+              'number', ra.number
+          )
+          ELSE '{}'::json
+      END AS address,
+      CASE 
+          WHEN COUNT(oh.*) > 0 THEN
+              COALESCE(json_agg(
+                  json_build_object(
+                      'weekday', oh.weekday,
+                      'start_time', oh.start_time,
+                      'end_time', oh.end_time
+                  )
+              ), '[]'::json)
+          ELSE '[]'::json
+      END AS opening_hours
+    FROM restaurants r
+    LEFT JOIN restaurant_addresses ra ON r.id = ra.restaurant_id
+    LEFT JOIN opening_hours oh ON r.id = oh.restaurant_id
+    WHERE r.id = $1
+    GROUP BY r.id, ra.id;
+    `,
       [id]
     );
-
 
     const [ restaurant ] = restaurants;
 
